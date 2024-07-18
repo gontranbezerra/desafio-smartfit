1f3e9f1c724e2c660ca67f4980e15a25f34c07e5
gontranbezerra <gontranbezerra@gmail.com>
2024-07-18 09:43:08 -0300

    feat: form controls

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,17 +1,33 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  signal
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.scss',
 })
 export class FormComponent {
   results = signal<number[]>([]);
+
+  private nnfb = new FormBuilder().nonNullable;
+
+  public formGroup = this.nnfb.group({
+    hour: ['', Validators.required],
+    showClosed: [false, Validators.required],
+  });
+
+  onSubmit = () => {
+    this.printForm();
+  };
+
+  onClean = () => {
+    this.formGroup.reset();
+  };
+
+  protected printForm() {
+    console.log(this.formGroup.value);
+  }
 }
