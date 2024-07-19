9d4259284aa21340b1dc1e1a002977424346a7b8
gontranbezerra <gontranbezerra@gmail.com>
2024-07-19 06:13:57 -0300

    fix: json-server CORS setting

---
+++ src/app/data/server.js
@@ -0,0 +1,33 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const middlewares = jsonServer.defaults();
+const data = require("./locations.json");
+
+server.use(middlewares);
+
+// Add custom middleware for CORS
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // Allow any origin
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+
+  next();
+});
+
+server.use((req, res, next) => {
+  setTimeout(next, 500);
+});
+
+server.get("/locations", (req, res) => {
+  res.jsonp(data.locations);
+});
+
+server.listen(3000, () => {
+  console.log("JSON Server is running");
+});
