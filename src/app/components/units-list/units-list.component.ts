44d34433085a3b5ad4800521e178b4abefe7a92e
gontranbezerra <gontranbezerra@gmail.com>
2024-07-22 09:41:13 -0300

    fix: units list layout

--- src/app/components/units-list/units-list.component.ts
+++ src/app/components/units-list/units-list.component.ts
@@ -1,22 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 
+import { UnitCardComponent } from '@components/unit-card/unit-card.component';
+
 import { UnitLocation } from '@models/unit';
 
 @Component({
   selector: 'app-units-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, UnitCardComponent],
   templateUrl: './units-list.component.html',
   styleUrl: './units-list.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class UnitsListComponent {
-  // private unitState = inject(UnitState);
-
-  // protected units$: Observable<UnitLocation[]> = this.unitState.units$;
-
-  // units = toSignal(this.units$, { initialValue: [] });
-
   units = input.required<UnitLocation[]>();
 }
